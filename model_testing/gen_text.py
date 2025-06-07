8f30958f88c72070a95b13f0a624d4527e5a6d2b
sh2099 <samhewson16@gmail.com>
2025-06-07 18:01:02 +0200

    neaten methods and label arguments

--- model_testing/gen_text.py
+++ model_testing/gen_text.py
@@ -1,13 +1,8 @@
 import json
 import torch
 
-from ml.bigram_model import BigramModel
 
-model_path = 'trained_models/bigram_model.pth'
-vocab_path = 'trained_models/bigram_vocab.json'
-
-
-def load_model_and_vocab(model_path, vocab_path):
+def load_model_and_vocab(model_class: torch.nn.Module, n: int, model_path: str, vocab_path: str) -> tuple:
     """
     Load the trained model and vocabulary from the specified paths.
     """
@@ -15,48 +10,61 @@ def load_model_and_vocab(model_path, vocab_path):
         vocab_data = json.load(f)
     word2idx = vocab_data['word2idx']
     idx2word = vocab_data['idx2word']
-    # Conver idx2word keys to integers
+    # Convert idx2word keys to integers
     idx2word = {int(k): v for k, v in idx2word.items()}
     # Initialise the model
-    model = BigramModel(vocab_size=len(word2idx))
+    model = model_class(vocab_size=len(word2idx), n=n)
     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     return model, word2idx, idx2word
 
 
-def generate_text(model, start_words, word2idx, idx2word, n=2, max_length=50):
-    print(f"Prompt: {' '.join(start_words)}")
+def generate_text_n_gram(model: torch.nn.Module, 
+                         prompt: str, 
+                         word2idx: dict, 
+                         idx2word:dict, 
+                         max_length: int = 50
+                         ) -> None:
+    print(f"Prompt: {prompt}")
+    start_words = prompt.split()  # Split the prompt into words
     model.eval()
     generated_text = start_words.copy()
+    k = model.context_size  
     for _ in range(max_length - len(start_words)):
         # Prepare input tensor from the last n words
         #TODO: Add padding if the length of generated_text is less than n
         # Probably a good idea to add padding to vocab
-        input_tensor = torch.tensor([word2idx[word] for word in generated_text[-n+1:]], dtype=torch.long)
-
-        # Add batch dimension
-        #input_tensor = input_tensor.unsqueeze(0)
-
+        input_tensor = torch.tensor([word2idx[word] for word in generated_text[-k:]], dtype=torch.long)
+        #print(input_tensor, input_tensor.shape)
+            # Change this into logging
+        #Add batch dimension
+        input_tensor = input_tensor.unsqueeze(0)
+        #print(input_tensor, input_tensor.shape)
+            # Again logging maybe
         # Get model predictions
         with torch.no_grad():
             predictions = model(input_tensor)
-            #print(f"Predictions: {predictions}, len: {len(predictions)}")
-            pred_label = torch.argmax(predictions, dim=1).item()
-            generated_text.append(idx2word[pred_label])
-
+            _, pred_label = torch.max(predictions, dim=1)
+            #pred_label = torch.argmax(predictions, dim=1).item()
+            generated_text.append(idx2word[pred_label.item()])
+            
     print(' '.join(generated_text))
 
 
 def main():
     # Load the model and vocabulary
-    model, word2idx, idx2word = load_model_and_vocab(model_path, vocab_path)
+    from ml.n_gram_model import NGramModel
+    n = 4
+    model_path = f'trained_models/{n}-gram_model.pth'
+    vocab_path = f'trained_models/{n}-gram_vocab.json'
+    model, word2idx, idx2word = load_model_and_vocab(NGramModel, n, model_path, vocab_path)
     #print(idx2word[2292])
 
     # Example usage
-    start_words = ['the', 'man']
-    n = 2  # Bigram
-    max_length = 50  # Generate up to 50 words
+    start_prompt = 'he turned over the' 
+    start_words = start_prompt.split()  # Split the prompt into words
+    max_length = 300  # Generate up to 50 words
 
-    generate_text(model, start_words, word2idx, idx2word, n=n, max_length=max_length)
+    generate_text_n_gram(model, start_words, word2idx, idx2word, max_length=max_length)
 
 if __name__ == "__main__":
     main()
